f1ac674e562c92f521455a406c51bf4096fb5a35
nilSinger <niilz@outlook.de>
2021-12-29 16:44:16 +0100

    Initial commit: Can upload video and display in video element

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useRef, useState, useEffect } from "react";
 
 function App() {
+  const [videoFile, setVideoFile] = useState(null);
   return (
     <div className="App">
       <header className="App-header">
@@ -18,8 +20,24 @@ function App() {
           Learn React
         </a>
       </header>
+      <main>
+        <input
+          id="video"
+          type="file"
+          onInput={(e) => setVideoFile(e.target.files[0])}
+        />
+        <video
+          src={videoFile && window.URL.createObjectURL(videoFile)}
+          controls
+        />
+        <button onClick={splitVideo}>split</button>
+      </main>
     </div>
   );
 }
 
 export default App;
+
+function splitVideo() {
+  const video = document.querySelector("video");
+}
