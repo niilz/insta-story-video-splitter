d7e4542cc28b686c0d5ccc6ac83468852dd8fecc
nilSinger <niilz@outlook.de>
2021-12-31 01:28:31 +0100

    Make interval work like a loop (BUG: only saves the first file)

--- main.js
+++ main.js
@@ -2,48 +2,62 @@
 const input = document.querySelector("input");
 input.addEventListener("input", displayVideo);
 
+// Split the video when the button is pressed
+const video = document.querySelector("video");
+
 // Display the uploaded video in the video element
 function displayVideo(e) {
   const videoFile = e.target.files[0];
   video.src = videoFile && window.URL.createObjectURL(videoFile);
 }
 
-// Split the video when the button is pressed
-const video = document.querySelector("video");
 const splitButton = document.querySelector("#split-button");
 splitButton.addEventListener("click", splitVideo);
 
-async function splitVideo() {
-  const stream = video.captureStream();
-  console.log("stream", stream);
-  let data = [];
-  const recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
-
-  recorder.ondataavailable = (event) => data.push(event.data);
-  recorder.start();
-  console.log("recorder started");
+function splitVideo() {
   video.play();
   console.log("started video");
 
-  recorder.onstop = async (e) => {
-    console.log("recorder stopped");
+  let i = 1;
+
+  const splitLoop = setInterval(() => {
+    const stream = video.captureStream();
+    console.log("stream", stream);
+
+    let data = [];
+    const recorder = new MediaRecorder(stream);
+
+    recorder.ondataavailable = (event) => data.push(event.data);
+
+    recorder.start();
+    console.log("recorder started");
+
+    recorder.onstop = (_e) => {
+      download(data, i++);
+    };
+
+    setTimeout(() => recorder.stop(), 3000);
+  }, 3000);
+
+  setTimeout(() => clearInterval(splitLoop), 10000);
+
+  const download = (data, num) => {
+    console.log(num);
+    console.log("data download", data);
+
+    const objUrl = URL.createObjectURL(data[0]);
+
+    let downloadable = document.createElement("a");
+    downloadable.href = objUrl;
+    const fileName = `test${num}.webm`;
+    downloadable.download = fileName;
+
+    document.querySelector("body").appendChild(downloadable);
+    downloadable.innerHTML = fileName;
+    downloadable.click();
+
+    console.log("donwloadable", downloadable);
+
+    URL.revokeObjectURL(data[0]);
   };
-
-  const downloadLoop = setInterval(() => download(data), 1500);
-  setTimeout(() => clearInterval(downloadLoop), 3000);
-
-  recorder.stop();
-}
-
-function download(data) {
-  console.log("data", data);
-
-  const objUrl = URL.createObjectURL(data[0]);
-
-  let downloadable = document.createElement("a");
-  downloadable.href = objUrl;
-  downloadable.download = "test.webm";
-  downloadable.click();
-
-  URL.revokeObjectURL(data[0]);
 }
